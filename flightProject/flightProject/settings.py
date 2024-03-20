ed90276490b96a87716af63894b8f527db00c0fc
suprajashetty123 <108724602+suprajashetty123@users.noreply.github.com>
2024-03-20 18:06:30 +0530

    Update settings.py

--- flightProject/flightProject/settings.py
+++ flightProject/flightProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mh1htxk2f7x20gs6r2ig@8e&e&=ubu-!cjh_o*5h1q!-06&u81'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
